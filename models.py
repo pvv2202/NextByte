ce2f14b2773071db2410b2395323a959db4d144e
FynnHR28 <fynnrhr@gmail.com>
2025-04-16 13:23:15 -0400

    restructuring files

--- models.py
+++ models.py
@@ -2,53 +2,36 @@ import torch
 import torch.nn 
 import numpy as np 
 
+   
+class DecoderLayer(nn.Module):
+    """masked mh attention -> add norm -> feedforward -> add norm"""
+    def __init__(self, d_model, num_heads):
+        super(DecoderLayer, self).__init__()
+        self.masked_mh_attention = nn.MultiHeadAttention(d_model, num_heads)
+        #
+        self.attn_mask = None # TODO: would this be a torch.tril matrix of with the upper triangle zeroed out?
+        # layer norm applies to each embedding vector individual, so it needs the size of those vectors (d_model)
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.ffn = 
+        
+        pass
 
-
-class PositionalEncoder(nn.Module):
-    """PARAMS:
-    
-        context length (int): the max number of tokens the model can process at once
-        
-        d_model (int): length of a token's embedding vector, referred to generally as the 'model dimension'
-        
-        pdrop, float (Optional): probability of zeroing out an input, default to 0.1
-    """
-    def __init__(self, context_len, d_model, pdrop=0.1):
-        super.__init__()
-        # encode each position (context_len) with d_model dimensions
-        self.pe = torch.zeros(context_len, d_model) # shape: (context_len x d_model) 
-        
-        self.dropout = nn.Dropout(p=pdrop)
-        
-        # create a tensor holding every possible position in the input sequence
-        position = torch.arrange(0, context_len, dtype=torch.float).unsqueeze(1) # shape: (context_len x 1)
-        
-        # by a bunch of annoying log properties this expression is equal to the dividing term
-        # in the paper, and allows for more numerical stability, removing the need to calculate
-        # exponents with 10000 as the base
-        # 2i just means all the even numbers in d_model the torch.arrange below accomplishes that
-        div_term = torch.exp(-1 * (torch.arrange(0, d_model, 2) / d_model) * torch.log(10000.0))
-        
-        # apply position * div term to every value in pe matrix, sin for even, cos for odd
-        self.pe[:, 0::2] = torch.sin(position * div_term) # for each row, start at col 0 & skip 2 (even)
-        self.pe[:, 1::2] = torch.cos(position * div_term) # for each row, start at col 1 and skip 2 (odd)
-        self.pe.unsqueeze(0) # add a batch dimension,  shape: (1 x context_len x d_model)
-        
-        self.register_buffer('pe', self.pe) # fixes embeddings, if we want to have them learn we can change
-        
-    """input x should be a tensor of shape (batch_size, len_input_sequence, d_model)"""
     def forward(self, x):
-        # you slice x.shape(1) rows out of the positional encoding matrix to match the length of the input
-        # sequence explicitly, used in case input sequence length != context_len. I don't think this would 
-        # matter if we pad inputs to max length, but this should help if we decide not to
-        x = x + (self.pe[:, :x.shape[1], :]) 
+        """ forward method of a mhattention object needs q, k, v (it handles the projection of the inputs),
+            and an attention mask
+        """
         
-        # apply dropout to help with overfitting and return
-        return self.dropout(x)
+        attn_outputs = self.masked_mh_attention(x, x, x, attn_mask = self.attn_mask)
         
     
 
 class NextByteTransformer(nn.Module):
-    def __init__(self, vocab_size, d_model, hidden_layers, context_length):
-        super.__init__()
-        pass 
+    """there are probably more hyper params to add here"""
+    def __init__(self, vocab_size, d_model, hidden_layers, context_length, num_heads):
+        super(NextByteTransformer, self).__init__()
+        # create the simple embedding layer
+        self.emmbedding_layer = nn.Embedding(vocab_size, d_model)
+        self.positional_encoder_layer = PositionalEncoder(context_length, d_model)
+        
+        # TODO: decoder block with masked MHattention + add/norm + ffn + add/norm + linear -> softmax
+        
