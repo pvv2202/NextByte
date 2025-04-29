743aa0316cfe12c5b8806c5c58346935ef130f1f
Paul Vander Vort 25 <pvandervort25@hpc-login1.cm.cluster>
2025-04-29 11:01:02 -0400

    Fixed average loss calculation

--- all_training.py
+++ all_training.py
@@ -1,6 +1,7 @@
 import torch
 import kagglehub
 import torch.nn as nn
+import numpy as np
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from transformers import get_scheduler, PreTrainedTokenizerFast
@@ -148,7 +149,7 @@ for epoch in range(num_epochs):
         loss = loss_fn(logits, labels)
         
         # add loss to within epoch list
-        epoch_loss.append(loss.item())
+        epoch_loss.append(loss.detach())
 
         # update weights
         loss.backward()
@@ -158,7 +159,7 @@ for epoch in range(num_epochs):
 
      # evaluation after every epoch
     # stacks losses of all batches into a num_batches x 1 tensor, gets mean, and converts to py float
-    avg_loss_per_epoch = torch.stack(epoch_loss).mean()
+    avg_loss_per_epoch = np.mean(epoch_loss)
     
     print("TRAIN METRICS")
     f1_t, acc_t, _ = evaluate_model(model, train_dataloader, device=device) # returns f1, acc, avg_loss in that order
