b79cf72e197d937aad1d5c0ef20cbd7dadbfc3b6
FynnHR28 <fynnrhr@gmail.com>
2025-05-08 20:32:08 -0400

    final result finals saved

--- R-code/analysis.R
+++ R-code/analysis.R
@@ -12,14 +12,19 @@ all_res <- read.csv('./data/all_res.csv') |>
 seq_res <- read_csv('./data/seq_res.csv') |>
  mutate(model = 'seq')
 
+mix_res <- read.csv('./data/mix_res.csv') |>
+  mutate(model = 'mix')
+
+
+
 #===============================================================================
 # Analysis
 #===============================================================================
 
-combined <- bind_rows(all_res, seq_res) |>
+combined <- bind_rows(all_res, seq_res, mix_res) |>
   select(-trial)
 
-combined |>
+summary_stat <- combined |>
   group_by(model)|>
   summarize(
      n =n(),
@@ -38,10 +43,32 @@ levene_test(precision~model, data=combined)
 levene_test(recall~model,data=combined)
 levene_test(f1~model, data=combined)
 
-t.test(bleu~model, data = combined, var.equal = TRUE)
-t.test(precision~model, data = combined, var.equal = TRUE)
-t.test(recall~model, data = combined, var.equal = TRUE)
-t.test(f1~model, data = combined, var.equal = TRUE)
+aov_bleu <- aov(bleu~model, data=combined)
+summary(aov_bleu)
+
+aov_p <- aov(precision~model, data=combined)
+summary(aov_p)
+
+aov_r <- aov(recall~model, data=combined)
+summary(aov_r)
+
+aov_f1 <- aov(f1~model, data=combined)
+summary(aov_f1)
+
+
+
+TukeyHSD(aov_bleu)
+
+TukeyHSD(aov_p)
+
+TukeyHSD(aov_r)
+
+TukeyHSD(aov_f1)
+
+# t.test(bleu~model, data = combined, var.equal = TRUE)
+# t.test(precision~model, data = combined, var.equal = TRUE)
+# t.test(recall~model, data = combined, var.equal = TRUE)
+# t.test(f1~model, data = combined, var.equal = TRUE)
 
 # visualize
 
